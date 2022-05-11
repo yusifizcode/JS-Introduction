89e21cb4e3bb0895dbc5cb3ef0fe76054daf8970
yusifizcode <5ba0gh2@code.edu.az>
2022-05-11 11:31:05 +0400

    js introduction

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Task - 1
+
+var arr = [4,5,6,7,12,3,45];
+
+// function FindMaxNumber(arr){
+//     let max = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if (arr[i]>max) {
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+
+// var maxNumber = FindMaxNumber(arr);
+
+// console.log(maxNumber)
+
+
+// Task - 2
+
+// function SumEvenNumbers(arr){
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i]%2==0){
+//             sum+=arr[i];
+//         }
+//     }
+//     return sum;
+// }
+
+
+// var sumEven = SumEvenNumbers(arr);
+// console.log(sumEven);
+
+
+// Task - 3
+
+// do{
+//     var num1 = prompt("Ededi daxil edin :")    
+// }
+// while(isNaN(num1))
+
+
+// do{
+//     var num2 = prompt("Ededin quvvetini daxil edin :")    
+// }
+// while(isNaN(num2))
+
+// console.log(Math.pow(num1,num2))
